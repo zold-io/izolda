749b5feb1219db5bc9d769e66518ff23f76ae826
Yegor Bugayenko <yegor256@gmail.com>
2019-05-06 00:04:18 +0300

    #1 skeleton

---
+++ izolda/izolda/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  izolda
+//
+//  Created by be on 5/5/19.
+//  Copyright © 2019 Zerocracy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
